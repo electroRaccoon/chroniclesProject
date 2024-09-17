8377a573a926b8f1e96ce4dfbb8790fed05c0434
electroRaccoon <mathieu.ab@gmail.com>
2024-09-17 12:06:56 +0200

    whole app login et register setup

--- base/views.py
+++ base/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render,redirect
 
 # Create your views here.
+
+def welcome_view(request):
+    return render(request, 'base/base.html')
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Redirect to login after successful signup
+    else:
+        form = UserCreationForm()
+    return render(request, 'base/signup.html', {'form': form})
--- chronicles_project/urls.py
+++ chronicles_project/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from base import views  # Import the view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('base.urls')),  # Include your base app's URLs (including the welcome page)
+    path('accounts/', include('django.contrib.auth.urls')),  # Add this line for authentication URLs
 ]
