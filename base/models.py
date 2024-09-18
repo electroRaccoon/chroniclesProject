6d26b55ec6978758418e57f52e4a673fc6ab063a
electroRaccoon <mathieu.ab@gmail.com>
2024-09-18 12:04:57 +0200

    personnage model added

--- base/models.py
+++ base/models.py
@@ -1,48 +1,70 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Modèle de base pour tous les personnages
-class Character(models.Model):
-    HUMAN = 'HU'
-    CHANGELING = 'CH'
+class PersonnageBase(models.Model):
+    nom = models.CharField(max_length=100)
+    chronique = models.CharField(max_length=100)
     
-    CHARACTER_TYPES = [
-        (HUMAN, 'Humain'),
-        (CHANGELING, 'Changelin'),
-    ]
+    # Attributs
+    intelligence = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    astuce = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    resolution = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    force = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    dexterite = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    vigueur = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    presence = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    manipulation = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    calme = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    
+    # Compétences
+    informatique = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    artisanat = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    combat = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    conduite = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    erudition = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    investigation = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    medecine = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    occulte = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    politique = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    science = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    armes_a_feu = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    armes_blanches = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    athletisme = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    discretion = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    larcin = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    pilotage = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    survie = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    animaux = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    empathie = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    expression = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    intimidation = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    persuasion = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    relationnel = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    sagesse_de_la_rue = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    subterfuge = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
 
-    name = models.CharField(max_length=100)
-    age = models.PositiveIntegerField()
-    player = models.ForeignKey(User, on_delete=models.CASCADE)  # Lien vers l'utilisateur créateur
-    character_type = models.CharField(
-        max_length=2,
-        choices=CHARACTER_TYPES,
-        default=HUMAN,
-    )
+    class Meta:
+        abstract = True
 
     def __str__(self):
-        return f"{self.name} ({self.get_character_type_display()})"
+        return self.nom
 
-# Modèle spécifique pour les humains (actuellement vide, prêt à être étendu)
-class Human(Character):
-    pass
+class PersonnageHumain(PersonnageBase):
+    vice = models.CharField(max_length=50)
+    vertu = models.CharField(max_length=50)
+    
+    # Vous pouvez ajouter d'autres champs spécifiques aux humains ici
+    
+    class Meta:
+        verbose_name = "Personnage Humain"
+        verbose_name_plural = "Personnages Humains"
 
-# Modèle spécifique pour les changelins
-class Changeling(Character):
-    SEEMING_CHOICES = [
-        ('Fairest', 'Fairest'),
-        ('Ogre', 'Ogre'),
-        # Ajouter d'autres types de Seeming
-    ]
-    KITH_CHOICES = [
-        ('Artist', 'Artist'),
-        ('Soldier', 'Soldier'),
-        # Ajouter d'autres types de Kith
-    ]
-
-    seeming = models.CharField(max_length=50, choices=SEEMING_CHOICES)
-    kith = models.CharField(max_length=50, choices=KITH_CHOICES)
-
-    def __str__(self):
-        return f"{self.name} - {self.seeming} ({self.kith})"
-
+class PersonnageChangelin(PersonnageBase):
+    aiguille = models.CharField(max_length=50)
+    fil = models.CharField(max_length=50)
+    
+    # Vous pouvez ajouter d'autres champs spécifiques aux changelins ici
+    
+    class Meta:
+        verbose_name = "Personnage Changelin"
+        verbose_name_plural = "Personnages Changelins"
